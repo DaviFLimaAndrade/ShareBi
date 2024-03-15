1cb22f9629ffd66fd6a83052ab86f50b76157b0e
validName <>
2024-03-15 14:14:04 -0300

    primeira versao

---
+++ shareBi/src/components/sidebar/components/Brand.tsx
@@ -0,0 +1,14 @@
+import { Flex, useColorModeValue } from '@chakra-ui/react';
+import { HorizonLogo } from 'components/icons/Icons';
+import { HSeparator } from 'components/separator/Separator';
+
+export function SidebarBrand() {
+	let logoColor = useColorModeValue('navy.700', 'white');
+	return (
+		<Flex alignItems='center' flexDirection='column'>
+			<HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
+			<HSeparator mb='20px' />
+		</Flex>
+	);
+}
+export default SidebarBrand;
